8344a3e8110b854118ca255b333d092dc8fd1e96
BV-STORE-ONLINE <bvstore503@gmail.com>
2026-04-10 13:58:43 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
+// ================= CARRITO =================
+
+// contador del carrito
 let cartCount = 0;
 
+// función para agregar productos (se usará después en parte 4)
 function addToCart() {
     cartCount++;
     document.getElementById("cart-count").innerText = cartCount;
